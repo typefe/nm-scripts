7e017473bbe5f852783441dcb17d2088a695f6cb
Efecan Arat <efecanarat@gmail.com>
2025-01-06 00:54:23 +0300

    remove warp control, improve network query

--- warp_captive_login.sh
+++ warp_captive_login.sh
@@ -1,26 +1,16 @@
 #!/bin/bash
 
-output=$(nmcli -t -f active,ssid dev wifi | egrep '^yes' | cut -d\' -f2)
-warpStatus=$(warp-cli status | awk -F 'Status update: ' '{print $2}' | tr -d "\n")
+output=$(iwgetid -r)
 
-if [[ $output == "yes:ibbWiFi" ]]; then
+if [[ $output == "ibbWiFi" ]]; then
     nc -zw1 google.com 443;
     if [ $? -eq 0 ]; then
-        echo "Online"
+        echo "Online" >> /var/log/ibbwifi-autologin.log
     else
-        if [[ $warpStatus != "Disconnected. Reason: Manual Disconnection" ]]; then
-            echo "Warp is connected but no connection, might be need a captive login."
-            echo "Disabling Warp..."
-            warp-cli disconnect
-            echo "Trying to login..."
-            python3 /home/typefe/Desktop/w/PYTHON/Exercise/captive_auto_login/script.py
-            warp-cli connect
-        else
-            echo "Warp is not connected. Trying to login..."
-            python3 /home/typefe/Desktop/w/PYTHON/Exercise/captive_auto_login/script.py
-        fi
+        echo "Trying to login..." >> /var/log/ibbwifi-autologin.log
+        python3 /home/typefe/Desktop/w/PYTHON/Exercise/captive_auto_login/script.py
     fi
-
 else
-    echo "Not connected to ibbWifi, captive_login stopping to work."
+    echo "Not connected to ibbWifi, captive_login stopping to work." >> /var/log/ibbwifi-autologin.log
 fi
+
